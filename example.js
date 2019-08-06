b739bbb425852f9a5d87ea28f56ba08478322edd
Arnav Singh <36072753+itsarnavsingh@users.noreply.github.com>
2019-08-06 19:46:27 +0530

    Create example.js

---
+++ example.js
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title>Thanos Snap</title>
+	<meta charset="UTF-8" />
+  <style>
+    body {
+      font-family: sans-serif;
+    }
+
+    button {
+      min-width: 335px;
+      max-width: 200px;
+      min-height: 40px;
+      margin:10vw; 
+      border: 0;
+      outline: 0; 
+
+      border-radius: 5vw;
+      color: white;
+
+      /* pink to orange */
+      /* background: #ee0979;  */
+      background: linear-gradient(to right, #7adf5c, rgb(43, 240, 40) );
+
+      vertical-align: top;
+
+      cursor: pointer;
+    }
+  </style>
+</head>
+
+<body>
+
+		<button>snap</button>
+    
+	<script src="thanosSnap.js">
+	</script>
+  
+</body>
+
+</html>
